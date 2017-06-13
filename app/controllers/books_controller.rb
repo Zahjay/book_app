eead1d56a0244a63ab81f74088be4408bdddb182
zahra abdallah <zkon360@gmail.com>
2017-06-13 18:28:10 -0400

    Crud functionality

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,17 @@
 class BooksController < ApplicationController
 
+# find the views but only for the the actions listed
+before_action :find_book, only: [:show, :edit, :update,:destroy]
 
-def index   
+def index 
+  # this shows the book in descending order  
   @books = Book.all.order(created_at: :desc)
+end
+
+
+def show
+  # the action comment out below is executed in the find book function
+  # @book = Book.find(params[:id])  
 end
 
 def new 
@@ -19,12 +28,33 @@ def create
   end
 end
 
+def edit
+
+end
+
+def update
+   #This makes sure that the book is updated once the params are changed
+  if @book.update(book_params)
+    redirect_to book_path(@book)
+  else
+    render 'edit'
+    end   
+  end
+
+def destroy
+  @book.destroy 
+  redirect_to root_path
+end
+
 private
 
 def book_params
     params.require(:book).permit(:title, :description, :author)
 end
 
-
-
+# function for show
+def find_book
+  # this allows you to find a book through the book_id
+  @book = Book.find(params[:id])
+  end
 end
